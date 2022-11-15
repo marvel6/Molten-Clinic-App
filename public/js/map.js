cd3a4b9a1f66c319a3c44af2a96d6668eb815d7c
marvel6 <marvelloussolomon49@gmail.com>
2022-11-15 10:35:01 +0100

    new commit

---
+++ public/js/map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken =
+ 'pk.eyJ1IjoibWFydmVsNiIsImEiOiJjbDN0N2dhNjIwNjB1M2NxcWtmcTdsdXhxIn0.7A1eBgOSyyqki-FttiA9rA';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  zoom: 9,
+  center: [6.9980, 3.4737]
+});
+
